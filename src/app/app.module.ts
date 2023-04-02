d22d7679b39064e2a9c3455b7b46eca7364f3314
Metador1 <erickjimnez@rocketmail.com>
2023-04-02 02:03:05 -0600

    Avance menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RegistroComponent } from './registro/registro.component';
 import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router';
+import { Routes } from '@angular/router';
+
+
+const appRoutes:Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'menu', component: MenuPrincipalComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +26,9 @@ import { MenuPrincipalComponent } from './menu-principal/menu-principal.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import UsersJson from '../../assets/data.json';
+import { Router } from '@angular/router';
+
+interface USERS {
+  usuario : String;
+  contrasenia : String;
+}
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
+
 export class LandingPageComponent {
+  usu = '';
+  cont = '';
+  
+  constructor ( private router:Router){}
+
+  Users: USERS[] = UsersJson;
+  
+  //Valida el usuario
+  logear():void {
+    for (var i = 0; i < this.Users.length; i++ ){
+      if ( this.usu == this.Users[i].usuario && this.cont == this.Users[i].contrasenia){
+          console.log('Es el usurio!');
+          this.router.navigate(['/menu']);
+      }
+    }
+  }
 
 }
